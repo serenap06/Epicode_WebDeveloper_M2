5623ac7f6d27d183d2d29e19cf6f82c3117ef37c
serenap06 <168187800+serenap06@users.noreply.github.com>
2026-04-26 22:00:43 +0200

    W1D1

---
+++ W1D1/W1D1.js
@@ -0,0 +1,11 @@
+let algoritmo = 'Un algoritmo è un insieme di istruzioni messe insieme per ottenere un risultato. Tutto ciò che ci circonda può essere considerato un algoritmo come fare il caffè'
+console.log(algoritmo)
+
+let variabile = 'Una variabile è un dato a cui viene dichiarato un determinato valore, può essere dichiarato con let o const. Nel primo caso (let) la variabile può essere svuotata e modificata, nel secondo (const) la variabile non è modificabile poiché è una costante.'
+console.log(variabile)
+
+let undefined = 'Una variabile undefined è un tipo di dato dichiarato ma non definito, a cui verrà in seguito assegnato un valore che può essere di qualsiasi tipo.'
+console.log(undefined)
+
+let nullVsUndefined = 'La variabile null invece è un tipo di dato vuoto, si usa anche per svuotare variabili precedentemente definite.'
+console.log(nullVsUndefined)
