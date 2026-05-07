c033c38fad9881a22886caec185260c719bd0425
serenap06 <168187800+serenap06@users.noreply.github.com>
2026-05-07 18:57:18 +0200

    W2D1

---
+++ W2D1/W2D1.js
@@ -0,0 +1,76 @@
+//ESERCIZI SUGLI IF: 
+
+/* ESERCIZIO 1
+ Scrivi un algoritmo per trovare il più grande tra due numeri interi.
+*/
+
+const firstNumber = 8
+const secondNumber = 5
+
+if (firstNumber > secondNumber) {
+  console.log(firstNumber + ' è maggiore di ' + secondNumber)
+} else {
+  console.log(secondNumber + ' è maggiore di ' + firstNumber)
+}
+
+/*
+ESERCIZIO 2
+  Crea un blocco condizionale if/else per mostrare in console il messaggio corretto in ogni condizione.
+
+  num < 5 - mostra in console "Tiny"
+  num < 10 - mostra in console "Small"
+  num < 15 - mostra in console "Medium"
+  num < 20 - mostra in console "Large"
+  num >= 20 - mostra in console "Huge"
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+//ESERCIZI SUI CICLI:
+
+/* ESERCIZIO 3
+  Mostra i numeri da 0 a 10 (incluso) in ordine ascendente, ma evitando di mostrare i numeri 3 e 8 (suggerimento: ripassa l'uso di "continue").
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO 11
+  Scrivi un ciclo in JavaScript per iterare da 0 a 15. Per ciascun elemento, il ciclo deve controllare the il valore corrente sia pari o dispari, e mostrare il risultato in console.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+//ESERCIZI EXTRA NON OBBLIGATORI
+
+/* ESERCIZIO EXTRA 1
+  Scrivi un algoritmo per verificare che, dati due numeri interi, il valore di uno di essi sia 8 oppure se la loro addizione/sottrazione sia uguale a 8.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO EXTRA 2
+Stai lavorando su un sito di e-commerce. Stai salvando il saldo totale del carrello dell'utente in una variabile "totalShoppingCart".
+C'è una promozione in corso: se il totale del carrello supera 50, l'utente ha diritto alla spedizione gratuita (altrimenti la spedizione ha un costo fisso pari a 10).
+Crea un algoritmo che determini l'ammontare totale che deve essere addebitato all'utente per il checkout.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO EXTRA 3
+  Oggi è il Black Friday e viene applicato il 20% su ogni prodotto.
+  Modifica la risposta precedente includendo questa nuova promozione nell'algoritmo, determinando, usando l'algoritmo del codice precedente, se le spedizioni siano gratuite oppure no e e calcolando il totale.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/*  ESERCIZIO EXTRA 4
+  Usa un operatore ternaio per assegnare ad una variabile chiamata "gender" i valori "male" o "female".
+  La scelta deve essere basata sul valore di un'altra variabile booleana chiamata isMale.
+  Es. se isMale e' vero, il valore di gender deve essere "male"
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+/* ESERCIZIO EXTRA 5
+  Scrivi un algoritmo che iteri i numeri da 1 a 100, stampandoli in console. Se un valore tuttavia è multiplo di 3 (operatore modulo!), stampa al suo posto la parola "Fizz" e se il numero è multiplo di 5, stampa "Buzz". Se le condizioni si verificano entrambe, stampa "FizzBuzz".
+*/
